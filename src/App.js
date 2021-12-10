63db84c99cf3fa56101605beddd96ec6224b6e39
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-12-10 17:33:44 +0100

    routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+/* eslint-disable comma-dangle */
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  generatePath,
+} from "react-router-dom";
+import { Year } from "components/Year";
 import { SongsList } from "./components/SongsList";
 import { Start } from "./components/Start";
 import { SongIndex, SongName } from "./components/Song";
@@ -10,13 +18,16 @@ export const App = () => {
     <>
       <BrowserRouter>
         <h1>This is the Taylor Swift music data API.</h1>
+        <Link to={generatePath("/songs/index/:index", { index: 10 })}>
+          All songs
+        </Link>
         <Routes>
           <Route index path="/" element={<Start />} />
           <Route path="/songs" element={<SongsList />} />
-          <Route index path="/songs/index/:index" element={<SongIndex />} />
-          <Route index path="/songs/title/:name" element={<SongName />} />
-          <Route index path="/songs/album/:album" element={<Albums />} />
-          <Route index path="/songs/year/:year" element={<Albums />} />
+          <Route path="/songs/index/:index" element={<SongIndex />} />
+          <Route path="/songs/title/:name" element={<SongName />} />
+          <Route path="/songs/album/:album" element={<Albums />} />
+          <Route path="/songs/year/:year" element={<Year />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Year.js
+++ src/components/Year.js
@@ -3,22 +3,22 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export const Albums = () => {
-  const [albumName, setAlbumName] = useState([]);
+export const Year = () => {
+  const [releaseYear, setReleaseYear] = useState([]);
 
-  const { album } = useParams();
+  const { year } = useParams();
 
   useEffect(() => {
-    fetch(`https://taylor-swift-music.herokuapp.com/songs/album?album=${album}`)
+    fetch(`https://taylor-swift-music.herokuapp.com/songs/year?year=${year}`)
       .then((res) => res.json())
-      .then((json) => setAlbumName(json));
+      .then((json) => setReleaseYear(json));
   }, []);
 
   return (
     <>
       <h1>A specific song</h1>
       <p>The data displayed is a sample. There is more song data in the API.</p>
-      {albumName.map((song) => (
+      {releaseYear.map((song) => (
         <div key={song.index}>
           <h1 key={song.index}>{song.name}</h1>
           <h2>{song.album}</h2>
