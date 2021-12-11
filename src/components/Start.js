6d73dec895554c4797c08256146ebd289a156079
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-12-11 12:10:20 +0100

    testing styling

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Start = () => {
   return (
-    <>
+    <div className="center-text">
       <p>List of songs</p>
       <p>Information about specific songs</p>
       <p>All songs from an album</p>
       <p>All songs from a release year</p>
-    </>
+    </div>
   );
 };
